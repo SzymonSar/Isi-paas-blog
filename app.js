f23e92b19993ade0ec047039cfff05fc8dfe6bfb
Shime\Szymin <25110@edu.amw.gdynia.pl>
2025-05-27 12:46:46 +0200

    no message

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+
+//wyslanie html do klienta
 app.get("/", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
@@ -9,7 +11,26 @@ const server = app.listen(port, () => console.log(`Example app listening on port
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
+//pobieranie html z pliku
 const fs = require('fs');
 const path = require('path');
 
 const html = fs.readFileSync(path.join(__dirname, 'app.html'), 'utf-8');
+
+const { Pool } = require('pg');
+
+// Połączenie z bazą
+const pool = new Pool({
+  connectionString: process.env.LINK
+});
+
+// test bazy danych
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');  
+    res.json({ currentTime: result.rows[0].now });
+  } catch (err) {
+    console.error('Błąd zapytania:', err);
+    res.status(500).send('Błąd połączenia z bazą danych');
+  }
+});
