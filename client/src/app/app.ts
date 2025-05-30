c84ad9c603fc518e05094e9f1bf4ac939b033064
Shime\Szymin <25110@edu.amw.gdynia.pl>
2025-05-30 12:37:02 +0200

    no message

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Main } from './komponenty/main/main';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Main],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
