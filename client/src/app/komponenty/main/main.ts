8879f8937f73d6fd8572e1555b757ee745521ab2
Shime\Szymin <25110@edu.amw.gdynia.pl>
2025-06-02 16:43:19 +0200

    no message

--- client/src/app/komponenty/main/main.ts
+++ client/src/app/komponenty/main/main.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './main.html',
   styleUrl: './main.css'
 })
 export class Main {
-  
+  bazaurl = "https://isi-paas-blog.onrender.com/"
+  danein: any[] = []
+  user: string = "";
+  tytul: string = "";
+  zawartosc: string = "";
+  AxiosGet = async () => {
+    let client = axios.create({
+      baseURL: this.bazaurl
+    });
+    try {
+      const response = await client.get(`/get-db/`);
+      this.danein = response.data
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  AxiosPost = async () => {
+    let client = axios.create({
+      baseURL: this.bazaurl
+    });
+    const dane = {
+      user: this.user,
+      tytul: this.tytul,
+      zawartosc: this.zawartosc
+    }
+    try {
+      const response = await client.put(`/add-db`, JSON.stringify(dane), {
+      headers: { 'Content-Type': 'application/json' }
+    });
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  Dodaj(){
+    this.AxiosPost()
+  }
+
+
 }
